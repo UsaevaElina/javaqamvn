273bf7f01cfff8cac68ccba7b76b04ce459b3498
ElinaUsaeva <Usaeva1@yandex.ru>
2023-03-14 20:25:00 +0300

    Unit tests on a Maven project written using JUnit 5 for the calculate function

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,189 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class BonusServiceTest {
+
+    @Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForUnRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForUnRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForRegisteredBorderMinValue() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 16_666;
+        boolean registered = true;
+        long expected = 499;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForRegisteredBorderValue() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 16_667;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForRegisteredBorderMaxValue() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 16_668;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForUnRegisteredBorderMinValue() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 49900;
+        boolean registered = false;
+        long expected = 499;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForUnRegisteredBorderValue() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 50000;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForUnRegisteredBorderMaxValue() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 50001;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForUnRegisteredZero() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 0;
+        boolean registered = false;
+        long expected = 0;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForRegisteredZero() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 0;
+        boolean registered = true;
+        long expected = 0;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
+
